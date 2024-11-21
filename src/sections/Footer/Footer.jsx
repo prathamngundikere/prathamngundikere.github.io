cc0ced0b76a8ecc6848f53759095679ffb1731aa
Pratham N Gundikere <139988055+prathamngundikere@users.noreply.github.com>
2024-11-21 14:44:14 +0530

    Update Footer.jsx

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -6,6 +6,9 @@ function Footer() {
       <p>
         &copy; 2024 prathamngundikere <br />
         All rights reserved
+        <a href="src/assets/3.jpg" target="_blank" rel="noopener noreferrer">
+  Open Image
+</a>
       </p>
     </section>
   );
